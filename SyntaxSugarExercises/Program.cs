542c4a264c64a9f627300c3a3dfdfafcaa4120e7
jeremij94 <jeremij94@gmail.com>
2022-09-15 11:37:20 -0500

    Syntax Sugar Exercise First Commit

---
+++ SyntaxSugarExercises/Program.cs
@@ -0,0 +1,19 @@
+﻿//Exercise 1:
+//Given the following code, change the code to implement:
+//inferred typing
+//string interpolation
+//and the ternary operator
+//int answer = 4;
+//string response;
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is greater than or equal to nine";
+//}
+
+var answer = 4;
+var response= (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than nine";
+Console.WriteLine(response);
